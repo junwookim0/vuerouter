2e32a2b457fa128aa689c18a117257c8d3b77de4
kimjunwoo <ekdlskalrx98@naver.com>
2022-04-14 17:35:38 +0900

    1

---
+++ 김준우_애플리케이션test/test/src/views/HomeView.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <table>
+      <tr>
+        <td>번호</td>
+        <td>제목</td>
+        <td>클릭횟수(조회수)</td>
+      </tr>
+      <tr v-for="board in $store.state.board" :key="board.id">
+        <td>
+        {{board.id}}
+        </td>
+        <td @click="$router.push(`/board/${board.id}`)" >
+        {{board.title}}
+        </td>
+
+        <td>{{board.count}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+a {
+  color: #2c3e50;
+  text-decoration: none;
+
+}
+
+</style>
---
+++ 김준우_애플리케이션test/test/src/views/WriteView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="write">
+    <h1>글을 작성하는 공간 입니다</h1>
+    <form action="">
+    <input  v-model="title" type="text" size="28"> <br>
+    <textarea v-model="memo" cols="30" rows="10"></textarea> <br>
+    </form>
+    <button @click="addmemoMu"> 완료 </button>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data : function(){
+    return{
+      title : '',
+      memo: '',
+    }
+  },
+  methods : {
+    addmemoMu : function(){
+      this.$store.commit('addtitle', this.title);
+      this.$store.commit('addmemo', this.memo);
+      this.$router.push('/');
+    },
+  },
+}
+</script>
